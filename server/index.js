d88f825c76da63263891e94ac0ff19fbb4a7280a
vadimsharoikin <102527868+vadimsharoikin@users.noreply.github.com>
2022-06-15 13:09:19 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 const tasks = require("./routes/tasks");
 const connection = require("./db");
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+// use it before all route definitions
 
 connection();
 
 app.use(express.json());
-
+app.use(cors());
 app.use("/api/tasks", tasks);
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
+
